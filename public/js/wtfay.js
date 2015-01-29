735f5049fcd190e05b3740a9a3444cb5268ba01d
fpumir <fpumir@terredeweb.fr>
2015-01-29 11:32:28 +0100

    async function
    Signed-off-by: fpumir <fpumir@terredeweb.fr>

---
+++ public/js/wtfay.js
@@ -0,0 +1,14 @@
+function async(verb,url,datas,callback){
+	var self=this;
+	var xhr = new XMLHttpRequest();
+	  xhr.open(verb, url);
+		xhr.onload = function() {
+		  if(xhr.status === 200){
+		   callback.call(self,xhr);
+		  }else{
+		    console.log('error');
+		  }
+		}
+		xhr.setRequestHeader("X-Requested-With", "XMLHttpRequest");
+		xhr.send(datas);
+}
